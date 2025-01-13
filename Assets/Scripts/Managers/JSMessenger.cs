3f4a76dc5fde61240710c651d7d7dc416fa00862
joeperpetua <qwerty.perpetua@gmail.com>
2025-01-13 18:24:54 +0100

    sync state when login in

--- Assets/Scripts/Managers/JSMessenger.cs
+++ Assets/Scripts/Managers/JSMessenger.cs
@@ -1,4 +1,5 @@
 using System.Runtime.InteropServices;
+using Dojo;
 using Dojo.Starknet;
 using Newtonsoft.Json;
 using UnityEngine;
@@ -12,6 +13,7 @@ public struct AccountData
 
 public class JSMessenger : MonoBehaviour
 {
+    public WorldManager worldManager;
     public DojoWorker dojoWorker;
 
     [DllImport("__Internal")]
@@ -25,57 +27,62 @@ public class JSMessenger : MonoBehaviour
     [DllImport("__Internal")]
     private static extern void ExecuteCreatePlayer();
 
-    void Start ()
+    void Start()
     {
         // Send signal to JS app that GameObject is ready for communication
-        #if UNITY_WEBGL && !UNITY_EDITOR
-            WebGLReady();
-        #endif
+#if UNITY_WEBGL && !UNITY_EDITOR
+        WebGLReady();
+#endif
     }
 
     public void OpenConnection()
     {
-        #if UNITY_WEBGL && !UNITY_EDITOR
-            OpenConnectionPage();
-        #else
-            dojoWorker.SimulateControllerConnection("test_username");
-        #endif
+#if UNITY_WEBGL && !UNITY_EDITOR
+        OpenConnectionPage();
+#else
+        dojoWorker.SimulateControllerConnection("test_username");
+#endif
     }
 
     public void Disconnect()
     {
-        #if UNITY_WEBGL && !UNITY_EDITOR
-            ClearSession();
-        #else
-            dojoWorker.SimulateControllerDisconnection();
-        #endif
+#if UNITY_WEBGL && !UNITY_EDITOR
+        ClearSession();
+#else
+        dojoWorker.SimulateControllerDisconnection();
+#endif
     }
 
-    public void RegisterAccount(string payload)
+    public async void RegisterAccount(string payload)
     {
         AccountData accountData = JsonConvert.DeserializeObject<AccountData>(payload);
 
-        // We mock data because Controller handles the transaction execution, only the address is needed
-        Debug.Log(accountData.rpc);
         JsonRpcClient provider = new(accountData.rpc);
         Account account = new(
             provider,
-            new SigningKey("0x0"),
+            new SigningKey("0x0"), // mock cause we don't need it, controller takes care of it
             new FieldElement(accountData.address)
         );
 
         dojoWorker.provider = provider;
         dojoWorker.account = account;
 
-        ExecuteCreatePlayer();
+        // Run player creation if not present in world
+        string addressHash = EncodingService.GetPoseidonHash(dojoWorker.account.Address);
+        if (worldManager.Entity(addressHash) == null)
+        {
+            ExecuteCreatePlayer();
+        }
 
+        await dojoWorker.SyncLocalEntities();
         Debug.Log($"Registering player {accountData.username}  |  {accountData.address}");
     }
 
     /// <summary>
     ///  Called from JS when ExecuteCreatePlayer(); is returned
     /// </summary>
-    public void PlayerCreated() {
+    public void PlayerCreated()
+    {
         dojoWorker.InitLocalCurrentFloor();
     }
 
